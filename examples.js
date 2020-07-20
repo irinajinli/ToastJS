efe4658bdc2187ad526ff0a595ba2f06c64eca7e
Jean Li <jean.li@mail.utoronto.ca>
2020-07-20 15:10:34 -0600

    Added very primitive popup to example site

--- examples.js
+++ examples.js
@@ -1,7 +1,18 @@
 "use strict";
 
-// select DOM elements
-const testDiv = document.querySelector("#myId");
+const popupGen = new PopupGenerator();
 
-// test functions from toast.js
-const test = new PopupGenerator();
+// select buttons in DOM
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+
+// event listeners for buttons
+button1.addEventListener("click", makePopup1);
+
+function makePopup1(e) {
+  log("button1 clicked");
+  e.preventDefault();
+  popupGen.makePopup(button1.parentElement);
+}
