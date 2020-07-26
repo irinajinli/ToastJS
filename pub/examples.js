f5a7edcd2257c8be74fa5cf80cccdd45f7837d3a
Jean Li <jean.li@mail.utoronto.ca>
2020-07-26 16:10:00 -0600

    Reorganized examples.html. Added displayImportantPopup. Implemented closeTrigger.

--- pub/examples.js
+++ pub/examples.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const popupGen = new PopupGenerator();
-
 // select elements in DOM
 const base = document.querySelector("#base");
 const button1 = document.querySelector("#button1");
 // const button2 = document.querySelector("#button2");
 const buttonStyledBox = document.querySelector("#buttonStyledBox");
-const button3 = document.querySelector("#button3");
-const button5 = document.querySelector("#button5");
 const buttonTranslucentPopup = document.querySelector(
   "#buttonTranslucentPopup"
 );
 const textButton = document.querySelector("#textButton");
 const boxButton = document.querySelector("#boxButton");
+const closeableButton = document.querySelector("#closeableButton");
 const hoverButton = document.querySelector("#hoverButton");
+const timedButton = document.querySelector("#timedButton");
+const importantButton = document.querySelector("#importantButton");
 
 // first, some basic built-in popups
 const textPopup = new Popup(
+  base,
   "text",
   "This is a default text popup.",
   "click",
@@ -26,6 +26,7 @@ const textPopup = new Popup(
 textPopup.addOpenTrigger(textButton);
 
 const boxPopup = new Popup(
+  base,
   "box",
   "This is a default box popup.",
   "click",
@@ -33,115 +34,41 @@ const boxPopup = new Popup(
 );
 boxPopup.addOpenTrigger(boxButton);
 
+// popup which closes when clicked twice
+const closeablePopup = new Popup(
+  base,
+  "box",
+  "This is a default close-button popup.",
+  "click",
+  "close button"
+);
+closeablePopup.addOpenTrigger(closeableButton);
+
+const timedPopup = new Popup(
+  base,
+  "box",
+  "This is a timed popup.",
+  "click",
+  "time",
+  2000
+);
+timedPopup.addOpenTrigger(timedButton);
+
 // hover popup
 const hoverPopup = new Popup(
+  base,
   "box",
   "This is a hover popup.",
   "mouseover",
   "mouseleave"
 );
 hoverPopup.addOpenTrigger(hoverButton);
-hoverPopup.addCloseTrigger(hoverButton);
 
-// first, some basic built-in popups
-// const makePopup1 = popupGen.makePopupFunc(
-//   "text",
-//   "this is a basic click popup"
-// );
-
-// popupGen.triggerByClick(button1, makePopup1);
-
-// const makePopup2 = popupGen.makePopupFunc(
-//   "box",
-//   "this is a basic box popup",
-//   "#212F3D"
-// );
-
-// popupGen.triggerByClick(button2, makePopup2);
-
-// custom-styled popups
-// function makePopupStyledBox(e) {
-//   e.preventDefault();
-//   const popup = popupGen.makeBoxPopup(
-//     e,
-//     "this is a close button popup",
-//     "#212F3D"
-//   );
-
-//   // style popup
-//   popupGen.changeBackgroundColor(popup, "#212F3D");
-//   popupGen.changeTextColor(popup, "white");
-//   popupGen.addCloseButton(popup);
-// }
-
-// popupGen.triggerByClick(buttonStyledBox, makePopupStyledBox);
-
-// function makePopup3(e) {
-//   e.preventDefault();
-//   const popup = popupGen.makeBoxPopup(e, "this is a hover popup");
-//   // give popup a name so it's easily removable
-//   popup.id = "mouseOverPopup";
-
-//   // style popup
-//   popupGen.changeBackgroundColor(popup, "#212F3D");
-//   popupGen.changeTextColor(popup, "white");
-// }
-
-// function removePopup3(e) {
-//   e.preventDefault();
-//   const toRemove = document.querySelector("#mouseOverPopup");
-//   toRemove.remove();
-// }
-
-// popupGen.triggerByMouseover(button3, makePopup3);
-// popupGen.closeByMouseleave(button3, removePopup3);
-
-// function makePopup4(e) {
-//   e.preventDefault();
-//   const popup = popupGen.makeBoxPopup(
-//     e,
-//     "this popup disappears after 2 seconds",
-//     "#7FB3D5"
-//   );
-
-//   // style popup
-//   popupGen.changeBackgroundColor(popup, "#7FB3D5");
-//   popupGen.changeTextColor(popup, "white");
-//   popupGen.addCloseButton(popup);
-
-//   // make popup disappear automatically after 2 seconds
-//   popupGen.addTimer(2000, popup);
-// }
-
-// popupGen.triggerByClick(button4, makePopup4);
-
-// function makePopup5(e) {
-//   e.preventDefault();
-//   const popupAndGreyOut = popupGen.makeImportantPopup(
-//     "this is an important popup",
-//     "white",
-//     base
-//   );
-
-//   popupGen.addCloseImportantButton(popupAndGreyOut[0], popupAndGreyOut[1]);
-// }
-
-// popupGen.triggerByClick(button5, makePopup5);
-
-// function makeTranslucentPopup(e) {
-//   e.preventDefault();
-//   // make background popup for demo purposes
-//   const textPopup = popupGen.makeTextPopup(e, "translucency demo");
-//   popupGen.changeTextColor(textPopup, "red");
-
-//   const translucentPopup = popupGen.makeBoxPopup(
-//     e,
-//     "this is a translucent popup",
-//     "#212F3D"
-//   );
-
-//   popupGen.changeBackgroundColor(translucentPopup, "rgba(29, 29, 22, 0.70)");
-//   popupGen.changeTextColor(translucentPopup, "white");
-// }
-
-// popupGen.triggerByClick(buttonTranslucentPopup, makeTranslucentPopup);
+const importantPopup = new Popup(
+  base,
+  "important",
+  "This is an important popup.",
+  "click",
+  "close button"
+);
+importantPopup.addOpenTrigger(importantButton);
--- pub/toast.js
+++ pub/toast.js
@@ -1,17 +1,14 @@
 "use strict";
 const log = console.log;
 
-function PopupGenerator() {
-  this.popups = [];
-}
-
 class Popup {
-  constructor(layout, contents, openTrigger, closeTrigger) {
+  constructor(base, layout, contents, openTrigger, closeTrigger, ms) {
+    this.base = base;
     this.layout = layout;
     this.contents = contents;
     this.openTrigger = openTrigger;
     this.closeTrigger = closeTrigger;
-    this.domElement = null;
+    this.ms = ms; // optional; only for when closeTrigger is time
   }
 
   addOpenTrigger(element) {
@@ -19,26 +16,19 @@ class Popup {
     element.addEventListener(self.openTrigger, self.displayPopup(element));
   }
 
-  addCloseTrigger(element, toClose) {
-    const self = this;
-    try {
-      element.addEventListener(self.closeTrigger, self.domElement.remove());
-    } catch {
-      log("error");
-    }
-  }
-
   displayPopup(element) {
     const self = this;
     switch (self.layout) {
       case "text":
-        return self.displayTextPopup();
+        return self.displayTextPopup(element);
       case "box":
-        return self.displayBoxPopup();
+        return self.displayBoxPopup(element);
+      case "important":
+        return self.displayImportantPopup(element);
     }
   }
 
-  displayTextPopup() {
+  displayTextPopup(element) {
     const self = this;
     return function (event) {
       // find coordinates of event's target element
@@ -55,11 +45,30 @@ class Popup {
       popup.appendChild(document.createTextNode(self.contents));
       event.currentTarget.parentElement.appendChild(popup);
 
+      if (self.closeTrigger === "mouseleave") {
+        element.addEventListener(self.closeTrigger, function (e) {
+          e.preventDefault();
+          popup.remove();
+        });
+      } else if (self.closeTrigger === "close button") {
+        const closeButton = document.createElement("button");
+        closeButton.innerHTML = "close";
+        closeButton.className = "closeButton";
+        closeButton.addEventListener("click", function () {
+          closeButton.parentElement.remove();
+        });
+
+        popup.appendChild(closeButton);
+      } else if (self.closeTrigger === "time") {
+        setTimeout(function () {
+          popup.remove();
+        }, self.ms);
+      }
       return popup;
     };
   }
 
-  displayBoxPopup() {
+  displayBoxPopup(element) {
     const self = this;
     return function (event) {
       // find coordinates of event's target element
@@ -78,144 +87,68 @@ class Popup {
 
       event.currentTarget.parentElement.appendChild(popup);
 
+      if (self.closeTrigger === "mouseleave") {
+        element.addEventListener(self.closeTrigger, function (e) {
+          e.preventDefault();
+          popup.remove();
+        });
+      } else if (self.closeTrigger === "close button") {
+        const closeButton = document.createElement("button");
+        closeButton.innerHTML = "close";
+        closeButton.className = "closeButton";
+        closeButton.addEventListener("click", function () {
+          closeButton.parentElement.remove();
+        });
+
+        popup.appendChild(closeButton);
+      } else if (self.closeTrigger === "time") {
+        setTimeout(function () {
+          popup.remove();
+        }, self.ms);
+      }
+
       return popup;
     };
   }
-}
 
-PopupGenerator.prototype = {
-  // new
-  makePopup: function (layout, openTrigger, closeTrigger) {
-    const popup = new Popup(layout, openTrigger, closeTrigger);
-    return popup;
-  },
+  displayImportantPopup(element) {
+    const self = this;
+    return function (event) {
+      // create element to "grey out" background
+      const greyOut = document.createElement("div");
+      greyOut.className = "greyOut";
+      base.appendChild(greyOut);
 
-  newMakePopupFunc: function (popup) {
-    const popupFunc = function (e) {
-      e.preventDefault();
+      // create centered popup
+      const popup = document.createElement("div");
+      popup.className = "importantPopup";
+      popup.style.backgroundColor = "white";
+      popup.appendChild(document.createTextNode(self.contents));
 
-      popup.displayPopup();
-    };
-  },
+      if (self.closeTrigger === "mouseleave") {
+        element.addEventListener(self.closeTrigger, function (e) {
+          e.preventDefault();
+          popup.remove();
+          greyOut.remove();
+        });
+      } else if (self.closeTrigger === "close button") {
+        const closeButton = document.createElement("button");
+        closeButton.innerHTML = "close";
+        closeButton.className = "closeButton";
+        closeButton.addEventListener("click", function () {
+          closeButton.parentElement.remove();
+          greyOut.remove();
+        });
 
-  // some basic popup formats are included
-  makePopupFunc: function (type, contents, colour) {
-    const popupFunc = function (e) {
-      e.preventDefault();
-
-      if (type == "text") {
-        popupGen.makeTextPopup(e, contents);
-      } else if (type == "box") {
-        popupGen.makeBoxPopup(e, contents, colour);
+        popup.appendChild(closeButton);
+      } else if (self.closeTrigger === "time") {
+        setTimeout(function () {
+          popup.remove();
+          greyOut.remove();
+        }, self.ms);
       }
+
+      base.appendChild(popup);
     };
-
-    return popupFunc;
-  },
-
-  triggerByClick: function (element, popupFunc) {
-    element.addEventListener("click", popupFunc);
-  },
-
-  triggerByMouseover: function (element, popupFunc) {
-    element.addEventListener("mouseover", popupFunc);
-  },
-
-  closeByMouseleave: function (element, removeFunc) {
-    element.addEventListener("mouseleave", removeFunc);
-  },
-
-  makeTextPopup: function (event, contents) {
-    // find coordinates of event's target element
-    const x = window.scrollX + event.currentTarget.getBoundingClientRect().left;
-    const y =
-      window.scrollY + event.currentTarget.getBoundingClientRect().top + 50;
-
-    const popup = document.createElement("div");
-    // popup.style = "z-index: 2; position: absolute;";
-    popup.className = "textPopup";
-    popup.style.left = x + "px";
-    popup.style.top = y + "px";
-    popup.appendChild(document.createTextNode(contents));
-    event.currentTarget.parentElement.appendChild(popup);
-
-    return popup;
-  },
-
-  makeBoxPopup: function (event, contents, borderColor) {
-    // find coordinates of event's target element
-    const x = window.scrollX + event.currentTarget.getBoundingClientRect().left;
-    const y =
-      window.scrollY + event.currentTarget.getBoundingClientRect().top + 50;
-
-    // get event's target's parent
-    const targetParent = event.currentTarget.parentElement;
-
-    const popup = document.createElement("div");
-
-    // default style
-    popup.className = "boxPopup";
-    popup.style.left = x + "px";
-    popup.style.top = y + "px";
-    popup.style.border = "1px solid " + borderColor;
-    popup.appendChild(document.createTextNode(contents));
-
-    event.currentTarget.parentElement.appendChild(popup);
-
-    return popup;
-  },
-
-  makeImportantPopup: function (contents, color, base) {
-    // create element to "grey out" background
-    const greyOut = document.createElement("div");
-    greyOut.className = "greyOut";
-    base.appendChild(greyOut);
-
-    // create centered popup
-    const popup = document.createElement("div");
-    popup.className = "importantPopup";
-    popup.style.backgroundColor = color;
-    popup.appendChild(document.createTextNode(contents));
-
-    base.appendChild(popup);
-
-    return [popup, greyOut];
-  },
-
-  addTimer: function (ms, popup) {
-    setTimeout(function () {
-      popup.remove();
-    }, ms);
-  },
-
-  changeTextColor: function (popup, color) {
-    popup.style.color = color;
-  },
-
-  changeBackgroundColor: function (popup, color) {
-    popup.style.backgroundColor = color;
-  },
-
-  addCloseButton: function (popup) {
-    const closeButton = document.createElement("button");
-    closeButton.innerHTML = "close";
-    closeButton.className = "closeButton";
-    closeButton.addEventListener("click", function () {
-      closeButton.parentElement.remove();
-    });
-
-    popup.appendChild(closeButton);
-  },
-
-  addCloseImportantButton: function (popup, greyOut) {
-    const closeButton = document.createElement("button");
-    closeButton.innerHTML = "close";
-    closeButton.className = "closeButton";
-    closeButton.addEventListener("click", function () {
-      popup.remove();
-      greyOut.remove();
-    });
-
-    popup.appendChild(closeButton);
-  },
-};
+  }
+}
